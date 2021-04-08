7c332ef9e846d2aabefa01db1aa2120133606d45
canhthaomodule1c0221h2 <79823013+canhthaomodule1c0221h2@users.noreply.github.com>
2021-04-08 13:59:16 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+let message = ;prompt(login == 'Employee') ?
+    'Hello' :
+    (login == 'Director') ?
+        'Greetings' :
+        (login == '') ?
+            'No login' :
+            '';
+alert("message")
